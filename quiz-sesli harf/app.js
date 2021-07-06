e9a8abd82b356c3bd7e7ab777981efde2f6f7459
ZeynepE1 <81360699+ZeynepE1@users.noreply.github.com>
2021-07-06 22:57:06 +0300

    Add files via upload

---
+++ quiz-sesli harf/app.js
@@ -0,0 +1,19 @@
+// var kelime = [];
+// kelime = document.getElementById("word").innerHTML;
+// console.log(kelime);
+// // btn.onclick = function(kelime){
+// //     kelime.include = true;
+
+// // }
+let kelime = document.getElementById("word");
+var a = new String(kelime);
+// function myFunction(kelime) {
+//     var a = new String(kelime);
+//     for (let i = 0; i < a.length;i++){
+
+//     }
+    
+
+    
+  
+  console.log(a);
---
+++ quiz-sesli harf/console/app2.js
@@ -0,0 +1,28 @@
+// let kelime = prompt("bir kelime giriniz:");
+// var a = new String(kelime);
+// console.log(`${a}`);
+
+// function myFunction(a) {
+//     for (var i = 0; i <a.length; i++) {
+//         if (a[i]===("a" || "e"||)
+
+    
+// }
+var str = prompt("bir kelime giriniz:");;
+btn.onclick=function(str){
+    return console.log(str.replace(/i/g,"1"));
+}
+
+// var arr = str.split("");
+// console.log(arr);
+
+// function myFunction(a) {
+//     for (var i = 0; i <=arr.length; i++) {
+//         if (arr[i]===("a")
+//         return arr[i].replace("1");    
+        
+        
+//     }
+// }
+
+// console.log(myFunction(str));
